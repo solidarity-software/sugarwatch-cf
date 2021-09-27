6de18fb2bb31eb4f3bdcd9ddb8a41add5b7fb7ee
Michael Osuna Jr <mike@solidarity.software>
2021-09-27 15:59:25 -0700

    make register and oauth2 endpoints work

---
+++ functions/src/dexcom.ts
@@ -0,0 +1,111 @@
+import * as admin from "firebase-admin";
+
+interface AuthenticateOptions {
+  accessToken: string
+  expiresIn: number
+  refreshToken: string
+}
+
+/**
+ *
+ * @param {number} expiresIn
+ * @return {Date}
+ */
+function expiresInToRefreshTime(expiresIn: number): Date {
+  const refreshTime = new Date();
+  refreshTime.setTime(refreshTime.getTime() + (expiresIn - 600) * 1000);
+  return refreshTime;
+}
+
+/**
+ *
+ * @param {number} length
+ * @return {string} code
+ */
+function makeCode(length: number): string {
+  let result = "";
+  const characters = "abcdefghijklmnopqrstuvwxyz0123456789";
+  const charactersLength = characters.length;
+  for ( let i = 0; i < length; i++ ) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+}
+
+
+/**
+ *
+ */
+export class Dexcom {
+  id: string
+  /**
+   *
+   * @param {string} id
+   */
+  constructor(id: string) {
+    this.id = id;
+  }
+
+  /**
+   * @param {string} id df
+   */
+  static async register(id: string): Promise<string> {
+    const egvs = new Dexcom(id);
+    return await egvs.register();
+  }
+
+  /**
+   *
+   * @return {string} code
+   */
+  async register(): Promise<string> {
+    const code = makeCode(6);
+    await admin.firestore().collection("watches").add({
+      id: this.id,
+      code,
+    });
+
+    return code;
+  }
+  /**
+   *
+   * @param {string} code
+   * @param {any} options
+   * @return {void}
+   */
+  static async authenticate(
+      code: string,
+      options: AuthenticateOptions,
+  ): Promise<void> {
+    const watches = await admin.firestore()
+        .collection("watches")
+        .where("code", "==", code).get();
+
+    if (watches.size !== 1) {
+      throw new Error("Invalid Code");
+    }
+
+    for (const watch of watches.docs) {
+      const egvs = new Dexcom(watch.id);
+      await egvs.authenticate(options);
+    }
+  }
+
+  /**
+   *
+   * @param {AuthenticateOptions} options
+   */
+  async authenticate(options: AuthenticateOptions): Promise<void> {
+    const watches = await admin.firestore()
+        .collection("watches")
+        .where("id", "==", this.id).get();
+
+    for (const watch of watches.docs) {
+      await watch.ref.update({
+        access_token: options.accessToken,
+        refresh_time: expiresInToRefreshTime(options.expiresIn),
+        refresh_token: options.refreshToken,
+      });
+    }
+  }
+}
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,6 +1,8 @@
 import * as functions from "firebase-functions";
 import * as admin from "firebase-admin";
 import * as express from "express";
+import {Dexcom} from "./dexcom";
+import axios from "axios";
 
 const app = express();
 
@@ -9,14 +11,55 @@ app.use(express.urlencoded({extended: true}));
 
 admin.initializeApp();
 
-// https://sugarwatch.solidarity.software => sw.api
-// update watches with new egv
 export const api = functions.https.onRequest(async (request, response) => {
   return await app(request, response);
 });
 
-
-// registers watch
-app.post( "/ping", async ( req, res ) => {
+app.get( "/api/ping", async ( req, res ) => {
   res.end("PONG");
 });
+
+app.post( "/api/register", async ( req, res ) => {
+  console.log(req.body);
+  if (req.body == null || req.body.id == null) {
+    res.sendStatus(400);
+    res.end("invalid id");
+    return;
+  }
+
+  try {
+    const code = await Dexcom.register(req.body.id);
+    res.end(code);
+  } catch (_) {
+    res.sendStatus(500);
+    res.end();
+  }
+});
+
+app.get( "/api/oauth2", async ( req, res ) => {
+  const query: any = req.query;
+
+  const params = new URLSearchParams();
+
+  params.append("client_secret", functions.config().dexcom.client_secret);
+  params.append("client_id", functions.config().dexcom.client_id);
+  params.append("code", query.code);
+  params.append("grant_type", "authorization_code");
+  params.append("redirect_uri", functions.config().dexcom.redirect_uri);
+
+  try {
+    const data = await axios.post(`https://${functions.config().dexcom.host}/v2/oauth2/token`, params);
+    await Dexcom.authenticate(query.state, {
+      accessToken: data.data.access_token,
+      refreshToken: data.data.refresh_token,
+      expiresIn: data.data.expires_in,
+    });
+  } catch (e) {
+    console.log(e);
+    res.sendStatus(500);
+    res.end("unable to get token");
+    return;
+  }
+
+  res.end("Phone paired");
+} );
